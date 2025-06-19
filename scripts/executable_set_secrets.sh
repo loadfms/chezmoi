bcdf43c2019f95446aad8e8c8054d0d1c4450820
loadfms <loadfms@gmail.com>
2025-06-19 00:51:06 -0300

    feat: adding scripts

---
+++ scripts/executable_set_secrets.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -f ~/.config/secrets/gh_token ]; then
+    GH_TOKEN=$(<~/.config/secrets/gh_token)
+    export GITHUB_TOKEN="$GH_TOKEN"
+fi
+
+if [ -f ~/.config/secrets/open_ai ]; then
+    OPENAI_API_KEY=$(<~/.config/secrets/open_ai)
+    export OPENAI_API_KEY="$OPENAI_API_KEY"
+fi
+
+export GOPRIVATE=github.com/AcordoCertoBR
