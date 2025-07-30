a996a5b20f5114518e461181bf458abdde0724c8
loadfms <loadfms@gmail.com>
2025-07-30 11:34:35 -0300

    feat: update configs

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -17,6 +17,8 @@ opt.mouse = ''
 
 -- Editor
 opt.number = false
+opt.relativenumber = false
+opt.colorcolumn = ''
 opt.wrap = false
 opt.signcolumn = 'yes'
 opt.showmatch = true
@@ -25,7 +27,6 @@ opt.foldmethod = 'marker'
 opt.splitright = true
 opt.splitbelow = false
 opt.conceallevel = 0
-opt.colorcolumn = '120'
 opt.cursorline = true
 opt.expandtab = true
 opt.shiftwidth = 4
@@ -34,7 +35,6 @@ opt.smartindent = true
 opt.shortmess:append { c = true }
 opt.ignorecase = true
 opt.smartcase = true
-opt.relativenumber = false
 opt.scrolloff = 9999
 opt.guicursor = ""
 opt.termguicolors = true
