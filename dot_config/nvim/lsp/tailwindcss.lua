53651b3a3b1367c4a2f44db1f5eca92a37e515c0
loadfms <loadfms@gmail.com>
2025-08-22 15:15:26 -0300

    feat: update configs

--- dot_config/nvim/lsp/tailwindcss.lua
+++ dot_config/nvim/lsp/tailwindcss.lua
@@ -1,8 +1,11 @@
 local path = require("utils.path")
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
 return {
     cmd = { "tailwindcss-language-server", "--stdio" },
-    filetypes = { "html", "typescript", "typescriptreact", "tsx", "css", "scss", "less", "vue" },
+    filetypes = { "html", "css", "scss", "less", "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "svelte" },
     root_dir = path.find_root({
         "tailwind.config.js",
         "tailwind.config.ts",
@@ -10,4 +13,5 @@ return {
         "package.json",
         ".git"
     }),
+    capabilities = capabilities,
 }
